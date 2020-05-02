e403df25da0433586e5cc72370ec3fd6276c318f
Henry Javangwe <henrysparks1@gmail.com>
2020-05-02 23:13:17 +0200

    created an articles.forEach function that will iterate through the array and access our params and then return whatever we'll be requesting e.g article.title or article.date etc. created a second rough article inside the server.js file to see more than one article being rendered on the main/index page

--- server.js
+++ server.js
@@ -11,14 +11,24 @@ app.set('view engine', 'ejs');
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res)=>{
-    const articles =[{
-        title      : "In And About The Most Beautiful Holiday Destinations ",
-        author     : "Baki Sparks",
-        content    : " Here's an overview of the worlds most beautiful holiday destinations",
-        imageUrl   : "",
-        createdAt  : Date.now()
-
-    }]
+    const articles =[
+        {
+            title      : "In And About The Most Beautiful Holiday Destinations ",
+            author     : "Baki Sparks",
+            content    : " Here's an overview of the worlds most beautiful holiday destinations",
+            imageUrl   : "",
+            createdAt  : Date.now()
+        },
+        {
+            title      : " The World Wild Beauty",
+            author     : "Baki Sparks",
+            content    : " Here's an overview of the worlds most beautiful natural spaces",
+            imageUrl   : "",
+            createdAt  : Date.now()
+        }
+    ]
     res.render('index', {articles: articles});
 })
+
+
 app.listen(5000) 
