e67b65f7dc8b41bd9ff08c39f9792c5ef3a353e3
Henry Javangwe <henrysparks1@gmail.com>
2020-05-03 12:11:47 +0200

    created the form container inside our new articles.ejs. since we're creating a new article, i created a router.post and set the method to post inside the form. created a partial view that'll contain only the form fields.  because the form inputs for the new article and the ones for the edit article are the same, we'll not create the form fields in here directly. so we'll create one input file so we don't have to copy and paste it between our new and edit views.

--- routes/articles.js
+++ routes/articles.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router(); //this will give us a router that'll use to create views.
 
 
+
+
 router.get('/newArticle', (req, res)=>{
     res.render('articles/newArticle')//here we render a page that we'll place inside articles and we're going to call it newArticle.
 });
+
+router.post('/', (req, res)=> {
+
+})
 
 module.exports = router;
