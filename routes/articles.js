ed81f4f50ca7774a32ff0979d9d301b6cefd544f
Henry Javangwe <henrysparks1@gmail.com>
2020-05-03 09:44:03 +0200

    to be able to use a local stylesheet to style, I first imported the path module into the server.js so that we can define the path to the css(const path = require('path') then set up the view engine to app.use(express.static(path.join(__dirname, '/public')));. to get a proper layout and view of the date in the paceholder articles, I switched the Date.now() to new Date(). because i moved the index.ejs inside an articles folder i changed the ref in the res.render to articles/index. created a folder articles inside the views dir and ,oved the index.ejs inside it and also created a newArticle.ejs inside the articles folder. inside the articles.js i created a router /newArticle and then rendered the newArticle apge that i created (newArticle). inside index.ejs used bootstrap to organise the cards and the card content. created ejs variables and spit out content from the placeholder content to the view. created a newPost button with a ref to the /articles/newArticle.  )

--- routes/articles.js
+++ routes/articles.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router(); //this will give us a router that'll use to create views.
 
 
-router.get('/test', (req, res)=>{
-    res.send('in articles')
+router.get('/newArticle', (req, res)=>{
+    res.render('articles/newArticle')//here we render a page that we'll place inside articles and we're going to call it newArticle.
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const  articleRouter= require('./routes/articles')
-const app = express();
+const articleRouter= require('./routes/articles')
+const app = express()
+const path = require('path')
 
 
 
 // view engine setup
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Register routers 
 app.use('/articles', articleRouter)
@@ -16,18 +18,20 @@ app.get('/', (req, res)=>{
             title      : "In And About The Most Beautiful Holiday Destinations ",
             author     : "Baki Sparks",
             content    : " Here's an overview of the worlds most beautiful holiday destinations",
-            imageUrl   : "",
-            createdAt  : Date.now()
+            imageUrl1   : "https://images.unsplash.com/photo-1517926967795-31943e805dae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+            imageUrl2   : "https://images.unsplash.com/photo-1543731068-7e0f5beff43a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80",
+            createdAt  : new Date()
         },
         {
             title      : " The World Wild Beauty",
             author     : "Baki Sparks",
             content    : " Here's an overview of the worlds most beautiful natural spaces",
-            imageUrl   : "",
-            createdAt  : Date.now()
+            imageUrl1   : "https://images.unsplash.com/photo-1588189697996-df0739f187e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+            imageUrl2:"https://images.unsplash.com/photo-1587852769298-7dae101bc745?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+            createdAt  : new Date()
         }
     ]
-    res.render('index', {articles: articles});
+    res.render('articles/index', {articles: articles});
 })
 
 
